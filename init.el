1fe224adc181718830284ff860848ca36f06579a
xyguo_mac <xyguo.ubd@gmail.com>
2016-02-21 09:37:48 +0800

    Users can add their customized settings in ~/.emacs.d/lisp/init-local.el; Triggering evil-mode in init-local.el for myself.

--- init.el
+++ init.el
@@ -119,12 +119,12 @@
 ;;   (load custom-file))
 
 
-;; ;;----------------------------------------------------------------------------
-;; ;; Allow users to provide an optional "init-local" containing personal settings
-;; ;;----------------------------------------------------------------------------
-;; (when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
-;;   (error "Please move init-local.el to ~/.emacs.d/lisp"))
-;; (require 'init-local nil t)
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
+  (error "Please move init-local.el to ~/.emacs.d/lisp"))
+(require 'init-local nil t)
 
 
 ;; ;;----------------------------------------------------------------------------
--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,5 +1,4 @@
 (require-package 'evil)
 (setq evil-want-C-u-scroll t)
 (require 'evil)
-;; (evil-mode 1)
 (provide 'init-evil)
