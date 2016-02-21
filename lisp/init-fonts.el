e7d0aa3ad3a78a58e64e3c81414fdf8e1b314e45
xyguo_mac <xyguo.ubd@gmail.com>
2016-02-21 09:40:06 +0800

    Use default-text-scale to scale font size

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -1,5 +1,9 @@
-(setq window-system-default-frame-alist
-      '((x (font . "文泉驿等宽微米黑 11")) ;; if frame created on x display
-        (nil))) ;; if on term
+(require-package 'default-text-scale)
+(global-set-key (kbd "C-M-=") 'default-text-scale-increase)
+(global-set-key (kbd "C-M--") 'default-text-scale-decrease)
+
+;; (setq window-system-default-frame-alist
+;;       '((x (font . "文泉驿等宽微米黑 11")) ;; if frame created on x display
+;;         (nil))) ;; if on term
 
 (provide 'init-fonts)
