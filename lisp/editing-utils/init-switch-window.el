825b7fa4b8d97cd98066e39094e1c97c3c60da4b
xyguo_mac <xyguo.ubd@gmail.com>
2016-02-29 08:55:36 +0800

    replace the obsolete 'delete-other-window' with 'switch-window-then-delete'

--- lisp/editing-utils/init-switch-window.el
+++ lisp/editing-utils/init-switch-window.el
@@ -1,5 +1,5 @@
 (require-package 'switch-window)
 ;; (require 'switch-window-autoloads)
 (global-set-key (kbd "C-x o") 'switch-window)
-(global-set-key (kbd "C-x 1") 'delete-other-window)
+(global-set-key (kbd "C-x 1") 'switch-window-then-delete)
 (provide 'init-switch-window)
