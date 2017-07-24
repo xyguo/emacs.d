8a446e17b51fb956c7c91d23815437c187c4de7f
xyguo_mac <xyguo.ubd@gmail.com>
2017-07-24 10:48:58 +0800

    replace redundant ido-ubquitous with ido-completing-read+

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -6,12 +6,13 @@
 (setq ido-use-virtual-buffers t)
 
 ;; use ido-ubiquitous
-(require-package 'ido-ubiquitous)
+(require-package 'ido-completing-read+)
 (ido-ubiquitous-mode t)
 
 ;; use smex to handle M-x
 (require-package 'smex)
 (smex-initialize)
 (global-set-key "\M-x" 'smex)
+(global-set-key "\M-X" 'smex-major-mode-commands)
 
 (provide 'init-ido)
